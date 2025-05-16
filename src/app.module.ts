6bb71ea255ff49730639b60c2d2ab94d9918b7a8
knull-a <theqwertyd@gmail.com>
2025-05-16 18:28:00 +0500

    feat: configure app module with GraphQL, TypeORM, and module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { FightersModule } from './modules/fighters/fighters.module';
+import { EventsModule } from './modules/events/events.module';
+import { FightsModule } from './modules/fights/fights.module';
+import { RankingsModule } from './modules/rankings/rankings.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.get('database') as TypeOrmModuleOptions;
+      },
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    ScheduleModule.forRoot(),
+    FightersModule,
+    EventsModule,
+    FightsModule,
+    RankingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Setup Swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('MMA Platform API')
+    .setDescription('API documentation for the MMA Platform')
+    .setVersion('1.0')
+    .addTag('fighters', 'Fighter management endpoints')
+    .addTag('events', 'Event management endpoints')
+    .addTag('fights', 'Fight management endpoints')
+    .addTag('rankings', 'Ranking management endpoints')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
